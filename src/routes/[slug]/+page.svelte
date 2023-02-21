9b89c2a8400ae94342b2099c74e29ca7d8cc8569
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2023-02-21 12:58:19 +0100

    First commit

---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { page, navigating, updated } from "$app/stores";
+
+  console.log($page.params);
+</script>
+
+<p>
+  {JSON.stringify(
+    {
+      page: $page,
+      navigating: $navigating,
+      updated: $updated
+    },
+    null,
+    2
+  )}
+</p>
